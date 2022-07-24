7a81f904c13fd83e68933808326f3b1347b91887
Kabir Oberai <oberai.kabir@gmail.com>
2022-07-24 15:38:06 -0400

    Fix Linux build
    It's not worth adding swift-crypto for one function so we just use OpenSSL

--- Supersign/Sources/Supersign/GrandSlam/Anisette/ADIDataProvider.swift
+++ Supersign/Sources/Supersign/GrandSlam/Anisette/ADIDataProvider.swift
@@ -1,13 +1,18 @@
 import Foundation
-import CryptoKit
+import CSupersign
 
 enum HashingError: Error {
     case hashingFailed
 }
 
 private extension String {
-    func sha256() throws -> SHA256Digest {
-        SHA256.hash(data: Data(utf8))
+    func sha256() throws -> [UInt8] {
+        try [UInt8](unsafeUninitializedCapacity: sup_sha256_buf_size()) { buf, count in
+            guard sup_sha256(self, utf8.count, buf.baseAddress!) == 0 else {
+                throw HashingError.hashingFailed
+            }
+            count = buf.count
+        }
     }
 }
 
