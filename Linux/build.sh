ddcdfaa972d2d0b21ec959e16bd7d8015a1e94c5
Kabir Oberai <oberai.kabir@gmail.com>
2024-11-24 16:43:39 -0800

    linuxdeploy tweaks

--- Linux/build.sh
+++ Linux/build.sh
@@ -6,7 +6,7 @@ shopt -s nullglob
 cd "$(dirname "$0")"
 
 # installer version
-export VERSION="1.0.0"
+export LINUXDEPLOY_OUTPUT_VERSION="1.0.0"
 
 if ! command -v fusermount &>/dev/null; then
     # Docker doesn't support FUSE
@@ -20,7 +20,7 @@ swift build --package-path .. -c release --product SupersignCLI --static-swift-s
 bin="$(swift build --package-path .. -c release --show-bin-path)"
 strip "${bin}/SupersignCLI"
 
-curr_git_info="$(curl -sS https://api.github.com/repos/linuxdeploy/linuxdeploy/git/refs/tags/continuous)"
+curr_git_info="$(curl -fsSL https://api.github.com/repos/linuxdeploy/linuxdeploy/git/refs/tags/continuous)"
 
 if [[ -f staging/linuxdeploy/git.json ]]; then
     if ! cmp -s staging/linuxdeploy/git.json <(echo "$curr_git_info"); then
@@ -33,8 +33,8 @@ else
 fi
 
 if [[ ! -f staging/linuxdeploy/linuxdeploy.AppImage ]]; then
-    curl "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$(arch).AppImage" \
-        -Lo staging/linuxdeploy/linuxdeploy.AppImage
+    curl -fsSL "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$(arch).AppImage" \
+        -o staging/linuxdeploy/linuxdeploy.AppImage
     chmod +x staging/linuxdeploy/linuxdeploy.AppImage
     echo "$curr_git_info" > staging/linuxdeploy/git.json
 fi
@@ -42,7 +42,7 @@ fi
 mkdir -p staging/tmp/AppDir/usr/bin
 find "${bin}"/ -name '*.resources' -print0 | xargs -0 -I {} cp -a {} "${PWD}/staging/tmp/AppDir/usr/bin/"
 
-OUTPUT="staging/tmp/Supersign.AppImage" ./staging/linuxdeploy/linuxdeploy.AppImage \
+LDAI_OUTPUT="staging/tmp/Supersign.AppImage" ./staging/linuxdeploy/linuxdeploy.AppImage \
     --appdir staging/tmp/AppDir \
     --output appimage \
     -e "${bin}/SupersignCLI" \
