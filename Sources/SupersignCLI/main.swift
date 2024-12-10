76df34f16e802987a026ceb71cc1004a7b5df4c6
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-10 19:26:43 +0530

    wip

--- Sources/SupersignCLI/main.swift
+++ Sources/SupersignCLI/main.swift
@@ -4,8 +4,9 @@ import SupersignCLISupport
 
 // let app = Bundle.module.url(forResource: "Supercharge", withExtension: "ipa")!
 
-#warning("Improve persistence on Windows/Linux")
+#warning("Improve persistence mechanism")
 
+// for macOS, we can use KeychainStorage but we need to sign with entitlements.
 // for Windows, we could use dpapi.h or wincred.h.
 // for Linux, maybe use libsecret?
 // see https://github.com/atom/node-keytar
