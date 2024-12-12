f16c2c869dad7aa6d04dbf2355494597f0d4b0f2
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-12 23:19:17 +0530

    Improve cancellation handling

--- Sources/SupersignCLISupport/SupersignCommand.swift
+++ Sources/SupersignCLISupport/SupersignCommand.swift
@@ -21,16 +21,7 @@ public enum SupersignCLI {
 
     public static func run(configuration: Configuration, arguments: [String]? = nil) async throws {
         _config = configuration
-        do {
-            var command = try SupersignCommand.parseAsRoot(arguments)
-            if var asyncCommand = command as? AsyncParsableCommand {
-                try await asyncCommand.run()
-            } else {
-                try command.run()
-            }
-        } catch {
-            SupersignCommand.exit(withError: error)
-        }
+        await SupersignCommand.cancellableMain(arguments)
     }
 }
 
@@ -50,4 +41,35 @@ struct SupersignCommand: AsyncParsableCommand {
             RunCommand.self,
         ]
     )
+}
+
+extension ParsableCommand {
+    fileprivate static func cancellableMain(_ arguments: [String]? = nil) async {
+        let (canStart, cont) = AsyncStream.makeStream(of: Never.self)
+        let task = Task {
+            for await _ in canStart {}
+            guard !Task.isCancelled else { return }
+            do {
+                var command = try self.parseAsRoot()
+                if var asyncCommand = command as? AsyncParsableCommand {
+                    try await asyncCommand.run()
+                } else {
+                    try command.run()
+                }
+            } catch is CancellationError {
+                self.exit()
+            } catch {
+                self.exit(withError: error)
+            }
+        }
+
+        signal(SIGINT, SIG_IGN)
+        let source = DispatchSource.makeSignalSource(signal: SIGINT)
+        source.setEventHandler { task.cancel() }
+        source.resume()
+
+        cont.finish()
+
+        await task.value
+    }
 }
