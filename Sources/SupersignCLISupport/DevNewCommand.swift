aa9b7b186a3c6536c0f1b750c0923ec28f19a9d1
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-21 16:26:22 +0530

    Support - in package name

--- Sources/SupersignCLISupport/DevNewCommand.swift
+++ Sources/SupersignCLISupport/DevNewCommand.swift
@@ -12,20 +12,20 @@ struct DevNewCommand: AsyncParsableCommand {
     func run() async throws {
         let name = try await Console.promptRequired("Package name: ", existing: self.name)
 
-        // TODO: support '-' in package name, replace with _ or CamelCase in Swift identifiers.
         var allowedFirstCharacters: CharacterSet = ["_"]
         allowedFirstCharacters.insert(charactersIn: "a"..."z")
         allowedFirstCharacters.insert(charactersIn: "A"..."Z")
 
         var allowedOtherCharacters = allowedFirstCharacters
         allowedOtherCharacters.insert(charactersIn: "0"..."9")
+        allowedOtherCharacters.insert("-")
 
         // promptRequired validates !isEmpty
         let firstScalar = name.unicodeScalars.first!
         guard allowedFirstCharacters.contains(firstScalar) else {
             throw Console.Error("""
             Package name '\(name)' is invalid. \
-            The package name must start with [a-z, A-Z, or _]. Found '\(firstScalar)'.
+            The package name must start with one of [a-z, A-Z, _]. Found '\(firstScalar)'.
             """)
         }
 
@@ -33,7 +33,7 @@ struct DevNewCommand: AsyncParsableCommand {
             let invalidValue = name[firstInvalid]
             throw Console.Error("""
             Package name '\(name)' is invalid. \
-            The package name may only contain [a-z, A-Z, 0-9, and _]. Found '\(invalidValue)'.
+            The package name may only contain [a-z, A-Z, 0-9, _, -]. Found '\(invalidValue)'.
             """)
         }
 
@@ -44,6 +44,8 @@ struct DevNewCommand: AsyncParsableCommand {
         }
 
         print("Creating package: \(name)")
+
+        let moduleName = name.replacingOccurrences(of: "-", with: "_")
 
         let files: [(String, String)] = [
             (
@@ -63,13 +65,13 @@ struct DevNewCommand: AsyncParsableCommand {
                         // A Swiftpack project should contain exactly one library target,
                         // representing the main app.
                         .library(
-                            name: "\(name)",
-                            targets: ["\(name)"]
+                            name: "\(moduleName)",
+                            targets: ["\(moduleName)"]
                         ),
                     ],
                     targets: [
                         .target(
-                            name: "\(name)"
+                            name: "\(moduleName)"
                         ),
                     ]
                 )
@@ -113,12 +115,12 @@ struct DevNewCommand: AsyncParsableCommand {
             ),
 
             (
-                "Sources/\(name)/\(name)App.swift",
+                "Sources/\(moduleName)/\(moduleName)App.swift",
                 """
                 import SwiftUI
                 
                 @main
-                struct \(name)App: App {
+                struct \(moduleName)App: App {
                     var body: some Scene {
                         WindowGroup {
                             ContentView()
@@ -129,7 +131,7 @@ struct DevNewCommand: AsyncParsableCommand {
             ),
 
             (
-                "Sources/\(name)/ContentView.swift",
+                "Sources/\(moduleName)/ContentView.swift",
                 """
                 import SwiftUI
                 
