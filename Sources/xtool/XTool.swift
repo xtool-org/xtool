572fc897bacfbf8017c08ae558d0853cf80ae92e
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-09 18:24:56 +0530

    Package xtool as a .app on macOS

--- Sources/xtool/XTool.swift
+++ Sources/xtool/XTool.swift
@@ -9,13 +9,9 @@ enum XToolMain {
     static func main() async throws {
         prepareDependencies { dependencies in
             #warning("Improve persistence mechanism")
-            // for macOS, we can use KeychainStorage but we need to sign with entitlements.
             // for Windows, we could use dpapi.h or wincred.h.
             // for Linux, maybe use libsecret?
             // see https://github.com/atom/node-keytar
-//            #if os(macOS)
-//            dependencies.keyValueStorage = KeychainStorage(service: "com.kabiroberai.xtool-keychain.credentials")
-//            #endif
         }
         try await XTool.run()
     }
