9ec9e6723c756628c98c2af30cbefcd19430f386
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-16 00:49:31 +0530

    Add update info to AppImage (#42)

--- Linux/build.sh
+++ Linux/build.sh
@@ -5,8 +5,8 @@ shopt -s nullglob
 
 cd "$(dirname "$0")"
 
-# installer version
-export LINUXDEPLOY_OUTPUT_VERSION="1.0.0"
+# release version
+export LINUXDEPLOY_OUTPUT_VERSION="latest"
 
 if ! command -v fusermount &>/dev/null; then
     # Docker doesn't support FUSE
@@ -21,6 +21,7 @@ bin="$(swift build --package-path .. -c release --show-bin-path)"
 strip "${bin}/xtool"
 
 curr_git_info="$(curl -fsSL https://api.github.com/repos/linuxdeploy/linuxdeploy/git/refs/tags/continuous)"
+curr_arch="$(uname -m)"
 
 if [[ -f staging/linuxdeploy/git.json ]]; then
     if ! cmp -s staging/linuxdeploy/git.json <(echo "$curr_git_info"); then
@@ -33,7 +34,7 @@ else
 fi
 
 if [[ ! -f staging/linuxdeploy/linuxdeploy.AppImage ]]; then
-    curl -fsSL "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$(arch).AppImage" \
+    curl -fsSL "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${curr_arch}.AppImage" \
         -o staging/linuxdeploy/linuxdeploy.AppImage
     chmod +x staging/linuxdeploy/linuxdeploy.AppImage
     echo "$curr_git_info" > staging/linuxdeploy/git.json
@@ -42,14 +43,18 @@ fi
 mkdir -p staging/tmp/AppDir/usr/bin
 find "${bin}"/ -name '*.resources' -print0 | xargs -0 -I {} cp -a {} "${PWD}/staging/tmp/AppDir/usr/bin/"
 
-LDAI_OUTPUT="staging/tmp/xtool.AppImage" ./staging/linuxdeploy/linuxdeploy.AppImage \
+env \
+LDAI_OUTPUT="staging/tmp/xtool.AppImage" \
+LDAI_UPDATE_INFORMATION="gh-releases-zsync|xtool-org|xtool|latest|xtool-${curr_arch}.AppImage.zsync" \
+    ./staging/linuxdeploy/linuxdeploy.AppImage \
     --appdir staging/tmp/AppDir \
     --output appimage \
     -e "${bin}/xtool" \
     -d xtool.desktop \
     -i xtool.png
 mkdir -p packages
-mv -f staging/tmp/xtool.AppImage packages/xtool.AppImage
+mv -f staging/tmp/xtool.AppImage "packages/xtool-${curr_arch}.AppImage"
+mv -f ./xtool.AppImage.zsync "packages/xtool-${curr_arch}.AppImage.zsync"
 
 rm -rf staging/tmp
 
