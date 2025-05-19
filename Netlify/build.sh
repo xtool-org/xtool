c7e71a0793543148cb5b51be067ee35dcc965415
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-20 02:55:13 +0530

    Use docc command directly

--- Netlify/build.sh
+++ Netlify/build.sh
@@ -6,7 +6,21 @@ cd "$(dirname "$0")"
 
 rm -rf docs
 
-swift package --allow-writing-to-package-directory \
-    generate-documentation --target XToolDocs --disable-indexing \
+if type -p xcrun &>/dev/null; then
+    docc="$(xcrun -f docc)"
+else
+    docc="docc"
+fi
+
+if [[ $# == 0 ]]; then
+    command="convert"
+elif [[ $# == 1 && ( "$1" == "preview" || "$1" == "convert" ) ]]; then
+    command="$1"
+else
+    echo "Usage: $0 [convert|preview]"
+    exit 1
+fi
+
+"$docc" "$command" xtool.docc \
     --experimental-enable-custom-templates \
     --output-path docs
