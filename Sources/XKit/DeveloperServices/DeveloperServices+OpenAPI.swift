1cd6dec731c7f689320d6fd01b22592272b509e2
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-22 21:54:26 +0530

    Handle XTL_DEV_LOG

--- Sources/XKit/DeveloperServices/DeveloperServices+OpenAPI.swift
+++ Sources/XKit/DeveloperServices/DeveloperServices+OpenAPI.swift
@@ -17,7 +17,8 @@ extension DeveloperAPIClient {
             ),
             transport: httpClient.asOpenAPITransport,
             middlewares: [
-                auth.middleware
+                auth.middleware,
+                LoggingMiddleware(),
             ]
         )
     }
@@ -217,5 +218,41 @@ public struct DeveloperAPIASCAuthMiddleware: ClientMiddleware {
         var request = request
         request.headerFields[.authorization] = "Bearer \(jwt)"
         return try await next(request, body, baseURL)
+    }
+}
+
+struct LoggingMiddleware: ClientMiddleware {
+    static let regex: NSRegularExpression? = {
+        guard let pat = ProcessInfo.processInfo.environment["XTL_DEV_LOG"] else { return nil }
+        return try? NSRegularExpression(pattern: pat)
+    }()
+
+    func intercept(
+        _ request: HTTPRequest,
+        body: HTTPBody?,
+        baseURL: URL,
+        operationID: String,
+        next: (HTTPRequest, HTTPBody?, URL) async throws -> (HTTPResponse, HTTPBody?)
+    ) async throws -> (HTTPResponse, HTTPBody?) {
+        var (response, body) = try await next(request, body, baseURL)
+
+        guard Self.regex?.firstMatch(
+                  in: operationID,
+                  range: NSRange(operationID.startIndex..., in: operationID)
+              ) != nil
+              else { return (response, body) }
+
+        print("\n\(operationID) response status -> \(response.status)")
+
+        if let unwrapped = body {
+            let data = try await Data(collecting: unwrapped, upTo: .max)
+            // body may only be consumable once, replace it with the collected data
+            body = .init(data)
+
+            let text = String(decoding: data, as: UTF8.self)
+            print("\(operationID) response body -> \(text)")
+        }
+
+        return (response, body)
     }
 }
