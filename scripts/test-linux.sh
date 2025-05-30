5ffeab7203b7094f2f925c329ad0cfecd6389332
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-30 21:16:49 +0530

    aa

--- scripts/test-linux.sh
+++ scripts/test-linux.sh
@@ -5,4 +5,16 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 cd "$(dirname "$0")"/..
 
 ls -la ./appimage
-./appimage/xtool-$(uname -m).AppImage --version
+cp ./appimage/xtool-$(uname -m).AppImage /usr/local/bin/xtool
+
+swift sdk install ./artifacts/sdk/darwin.artifactbundle
+
+mkdir /work
+cd /work
+
+xtool sdk install /Xcode.app
+
+xtool new Hello --skip-setup
+cd Hello
+
+xtool dev build --ipa
