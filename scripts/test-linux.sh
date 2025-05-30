e64b53a5299cde471e6fa56fc36a866a4cf55e71
Kabir Oberai <oberai.kabir@gmail.com>
2025-05-31 03:09:50 +0530

    tweaks

--- scripts/test-linux.sh
+++ scripts/test-linux.sh
@@ -2,17 +2,6 @@
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 
-cd "$(dirname "$0")"/..
-
-ls -la ./artifacts
-
-cp ./artifacts/xtool /usr/local/bin/xtool
-
-swift sdk install ./artifacts/darwin.artifactbundle.zip
-
-mkdir /work
-cd /work
-
 xtool new Hello --skip-setup
 cd Hello
 
