355ca955520c5f136db050fb1a185b56c48cf3a8
Kabir Oberai <oberai.kabir@gmail.com>
2025-06-19 20:14:36 -0400

    Fix ASC bug

---
+++ macOS/fastlane/spaceship_patch.rb
@@ -0,0 +1,17 @@
+require "spaceship"
+
+# the ASC API has started (June 2025) throwing a fit when it
+# sees DEVELOPER_ID_APPLICATION_G2 in the certificate types filter.
+# so we patch Spaceship to filter locally instead of via the API.
+# c.f. https://github.com/fastlane/fastlane/pull/29588
+
+class << Spaceship::ConnectAPI::Certificate
+  alias_method :orig, :all
+
+  def all(**args)
+    types = args[:filter]&.delete(:certificateType)&.split(",")
+    certs = orig(**args)
+    return certs unless types
+    certs.select { |cert| types.include?(cert.certificate_type) }
+  end
+end
