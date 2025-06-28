6af4579447cadcee516c68ef8c16c1bb92829036
Kabir Oberai <oberai.kabir@gmail.com>
2025-06-28 17:24:55 -0400

    cleanup

--- Sources/CXKit/mobileprovision.c
+++ Sources/CXKit/mobileprovision.c
@@ -12,28 +12,21 @@
 #include <openssl/bio.h>
 #include "mobileprovision.h"
 
-/// @brief A mobileprovision is a file used by iOS to authorize an app to use certain services.
 struct mobileprovision {
     PKCS7 *raw;
 };
 
-/// @brief Creates a new mobileprovision instance using a PKCS7 container.
 static mobileprovision_t mobileprovision_create(PKCS7 *raw) {
     mobileprovision_t profile = malloc(sizeof(struct mobileprovision));
-
-    if (!profile) {
-        return NULL;
-    }
+    if (!profile) return NULL;
 
     profile->raw = raw;
-
     return profile;
 }
 
 mobileprovision_t mobileprovision_create_from_data(const void *data, size_t len) {
     PKCS7 *raw = NULL;
     d2i_PKCS7(&raw, (const unsigned char **)&data, len);
-
     if (!raw) return NULL;
 
     return mobileprovision_create(raw);
@@ -44,32 +37,20 @@ void mobileprovision_free(mobileprovision_t profile) {
     free(profile);
 }
 
-/// @brief Copies the profile's data and returns it.
-/// @retval void* Data was successfully fetched.
-/// @retval NULL Data retrieval failed.
 void *mobileprovision_copy_data(mobileprovision_t profile, size_t *len) {
     unsigned char *data = NULL;
-
     size_t data_len = i2d_PKCS7(profile->raw, &data);
-    
     if (data_len < 0) return NULL;
-    
     *len = data_len;
     void *ret = malloc(data_len);
-    
+
     if (!ret) {
         OPENSSL_free(data);
-        data = NULL;
-        data_len = 0;
         return NULL;
     }
 
     memcpy(ret, data, data_len);
     OPENSSL_free(data);
-
-    data = NULL;
-    data_len = 0;
-
     return ret;
 }
 
@@ -77,15 +58,11 @@ const void *mobileprovision_get_digest(mobileprovision_t profile, size_t *len) {
     // we could dig into the struct ourselves instead but using the
     // documented API is better.
     BIO *digest_bio = PKCS7_dataDecode(profile->raw, NULL, NULL, NULL);
-
     if (!digest_bio) return NULL;
-
     char *data = NULL;
     long digest_len = BIO_get_mem_data(digest_bio, &data);
-
     // the mem pointer is owned by `profile`, not the bio
     BIO_free_all(digest_bio);
     if (len) *len = digest_len;
-
     return data;
 }
