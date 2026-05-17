b8b7963333e50c2938b9cad569085747b177c3bf
Kabir Oberai <oberai.kabir@gmail.com>
2026-05-17 15:51:28 -0400

    Shims

--- Sources/XUtils/System+Utils.swift
+++ Sources/XUtils/System+Utils.swift
@@ -4,6 +4,21 @@ public typealias FilePath = System.FilePath
 public typealias FileDescriptor = System.FileDescriptor
 #else
 import SystemPackage
+import Foundation
+
 public typealias FilePath = SystemPackage.FilePath
 public typealias FileDescriptor = SystemPackage.FileDescriptor
+
+extension URL {
+    public init?(filePath: FilePath) {
+        self.init(filePath: filePath.string)
+    }
+}
+
+extension FilePath {
+    public init?(_ url: URL) {
+        guard url.isFileURL else { return nil }
+        self.init(url.path)
+    }
+}
 #endif
