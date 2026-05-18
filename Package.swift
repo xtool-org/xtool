d5d9b8d02b9dc40fb05bbe40320fb7b2d2ea3eeb
Kabir Oberai <oberai.kabir@gmail.com>
2026-05-18 03:21:49 -0400

    Run tests on CI (#226)

--- Package.swift
+++ Package.swift
@@ -138,20 +138,6 @@ let package = Package(
                 "XToolSupport",
             ]
         ),
-        .testTarget(
-            name: "XKitTests",
-            dependencies: [
-                "XKit",
-                .product(name: "SuperutilsTestSupport", package: "xtool-core")
-            ],
-            exclude: [
-                "config/config-template.json",
-            ],
-            resources: [
-                .copy("config/config.json"),
-                .copy("config/test.app"),
-            ]
-        ),
         .target(
             name: "XToolSupport",
             dependencies: [
