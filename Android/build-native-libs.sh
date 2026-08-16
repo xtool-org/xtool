7e7b9e5df77d336dde0ac5770bc0e0cef2aeada3
Harry P <habs@sdf.org>
2026-08-16 17:14:14 -0400

    libimobiledevice: bionic has pthread_once in libc, not libpthread

--- Android/build-native-libs.sh
+++ Android/build-native-libs.sh
@@ -105,7 +105,9 @@ tar -C "$WORK" -xzf "$WORK/libimobiledevice.tar.gz"
 	# git-archive tarballs have no version info; provide one for bootstrap
 	git init -q . && git add -A && git -c user.email=ci@localhost -c user.name=ci commit -qm "libimobiledevice master snapshot"
 	echo "2.0.1-git" > .tarball-version
-	./autogen.sh --host="$TRIPLE" --prefix="$PREFIX" --without-cython
+	# bionic's pthreads are in libc; there is no libpthread, so the
+	# acx_pthread -lpthread probe fails spuriously. pthread_once exists.
+	ac_cv_func_pthread_once=yes ./autogen.sh --host="$TRIPLE" --prefix="$PREFIX" --without-cython
 	make -j"$(nproc)" install
 )
 
